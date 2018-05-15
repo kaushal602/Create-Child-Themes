386cfd837fbf561e469e296308157e4eaaa242b6
kaushal602 <32062745+kaushal602@users.noreply.github.com>
2018-05-15 11:39:11 +0530

    Create child-themes.php

---
+++ child-themes.php
@@ -0,0 +1,14 @@
+Pest this code in style.css
+
+/*
+Theme Name: ute child
+Theme URI: https://wordpress.org/themes/ute-child/
+Author: UTES Van Hire
+Author URI: http://www.igexsolutions.com/
+Template: ute
+Version: 1.1
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags:         light, dark, two-columns, right-sidebar, responsive-layout, accessibility-ready
+Text Domain:  ute-child
+*/
